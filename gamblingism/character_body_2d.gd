f0a742519b938e24c6153636a51f7da867d92ccb
DQ-Solo <ethan.y.lee7@gmail.com>
2025-09-28 14:35:44 -0400

    dewh

--- gamblingism/character_body_2d.gd
+++ gamblingism/character_body_2d.gd
@@ -8,10 +8,10 @@ func get_spawn() -> Transform2D:
 	return _spawn
 
 @onready var spawn_point: Node2D = $"../SpawnPoint"
-
+@onready var animated_sprite = $AnimatedSprite2D
 # --- Config ---
-const SPEED := 300.0
-const JUMP_VELOCITY := -400.0
+const SPEED := 150.0
+const JUMP_VELOCITY := -300.0
 const KILL_Y := 2000.0
 const DASH_MULT: float = 1.5     # speed multiplier while dash held
 var gravity : float = float(ProjectSettings.get_setting("physics/2d/default_gravity"))
@@ -40,19 +40,36 @@ func _physics_process(delta: float) -> void:
 		velocity.y += gravity * delta
 
 	# Jump
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	
+	if (Input.is_action_just_released("move right") or Input.is_action_just_released("move left")) and is_on_floor() and not (Input.is_action_pressed("move left") and (Input.is_action_pressed("move right"))):
+			animated_sprite.play("idle")
+		
 
 	# Horizontal move (rename to your actual actions!)
 	var dash := Input.is_action_pressed("dash")
-	if Input.is_action_pressed("move left"):
+	if Input.is_action_just_pressed("move left"):
 		velocity.x = -SPEED * (DASH_MULT if dash else 1.0)
-	elif Input.is_action_pressed("move right"):
+		if is_on_floor():
+			animated_sprite.play("leftRun")
+	elif Input.is_action_just_pressed("move right"):
 		velocity.x =  SPEED * (DASH_MULT if dash else 1.0)
+		if  is_on_floor():
+			animated_sprite.play("rightRun")
+			
+	
 	else:
 		velocity.x = 0.0
-
+		if is_on_floor():
+			animated_sprite.play("idle")
 	move_and_slide()
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP_VELOCITY
+		if Input.is_action_pressed("move right"):
+			animated_sprite.play("rightJump")
+		elif Input.is_action_pressed("move left"):
+			animated_sprite.play("leftJump")
+		else:
+			animated_sprite.play("staticJump")
 	
 func _kill_conditions():
 	if (global_position.y > KILL_Y 
@@ -76,3 +93,7 @@ func die() -> void:
 	#await get_tree().create_timer(0.6).timeout
 
 	emit_signal("died", self)  # Let the level/game manager react if needed
+
+
+func _on_animated_sprite_animation_changed(old_name: StringName, new_name: StringName) -> void:
+	pass # Replace with function body.
